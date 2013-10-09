5c57685a3b0f16f477518d171b461efff98e3c0f
Tugrul Savran <tugrul@Tugruls-MacBook-Pro.local>
2013-10-09 19:53:58 +0300

    Initial Commit

---
+++ WhoWants2BAMillionaire?/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WhoWants2BAMillionaire?
+//
+//  Created by Tugrul Savran on 10/9/13.
+//  Copyright (c) 2013 CuriouScientists. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
